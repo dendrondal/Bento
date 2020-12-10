3541003d3e776b50338e99ddf7a4676f5ab340b8
Dal Williams <dendrondal@protonmail.com>
2020-12-10 13:20:03 -0600

    First customizations

--- js/weather.js
+++ js/weather.js
@@ -9,7 +9,7 @@ weather.temperature = {
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = 'C';
+var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 43.389118;
+    let longitude = -71.097153;
 
     getWeather(latitude, longitude);
 }
